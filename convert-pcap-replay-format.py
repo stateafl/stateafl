46313312f500b526d56b414092b0a49bab94fd08
Roberto Natella <2117196+rnatella@users.noreply.github.com>
2022-05-10 21:12:58 +0200

    Improvements for conversion script

--- convert-pcap-replay-format.py
+++ convert-pcap-replay-format.py
@@ -9,12 +9,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--input', help="Input file (PCAP)", required=True)
 parser.add_argument('--server-port', type=int, help="Server port number", required=True)
 parser.add_argument('--output', help="Output file (AFLnet replay format)")
+parser.add_argument('--client-port', type=int, help="Client port number")
+parser.add_argument('--ignore-multiple-clients', default=False, action='store_true', help="Do not raise error if multiple clients are detected")
 
 args = parser.parse_args()
 
 
 PCAP = args.input
 SERVER_PORT=args.server_port
+CLIENT_PORT=args.client_port
 
 cap = None
 
@@ -30,12 +33,26 @@ if (SERVER_PORT < 0 or
     print("Error: Invalid server port number")
     sys.exit(1)
 
-
-
 if args.output is not None:
     OUTPUT = args.output
 else:
-    OUTPUT = os.path.splitext(os.path.basename(PCAP))[0] + ".out"
+    OUTPUT = os.path.splitext(os.path.basename(PCAP))[0] + ".replay"
+
+
+# Disable application-level protocol decoding
+# (we only take raw payload from TCP/UDP packets)
+app_protocols = {}
+
+for pkt in cap:
+    if pkt.highest_layer != "TCP" and pkt.highest_layer != "UDP":
+        app_protocols[pkt.highest_layer] = 1
+
+if len(app_protocols) > 0:
+    disabled_protocols = []
+    for k in app_protocols:
+        disabled_protocols.append('--disable-protocol')
+        disabled_protocols.append(k.lower())
+    cap = pyshark.FileCapture(PCAP, custom_parameters=disabled_protocols)
 
 
 total_messages = 0
@@ -64,14 +81,15 @@ try:
                     print("Please check that the PCAP file only contains traffic from/to SERVER_PORT")
                     sys.exit(1)
 
-                if len(ports) > 2:
+                if len(ports) > 2 and not(args.ignore_multiple_clients):
                     print("Error: Multiple client/server flows detected")
                     print("Please check that the PCAP file only contains traffic for only one client")
                     sys.exit(1)
 
-                if dstport == SERVER_PORT:
-                    if hasattr(pkt.tcp, 'payload'):
-                        request_msg.extend(pkt.tcp.payload.binary_value)
+                if (dstport == SERVER_PORT and
+                   (CLIENT_PORT is None or srcport == CLIENT_PORT)):
+                    if hasattr(pkt, 'data'):
+                        request_msg.extend(pkt.data.data.binary_value)
 
                 if (('TCP' in pkt and dstport != SERVER_PORT) or
                    ('UDP' in pkt)) and len(request_msg) > 0:
@@ -93,3 +111,4 @@ if total_messages == 0:
     sys.exit(1)
 
 print(f"Converted PCAP saved to {OUTPUT}")
+
